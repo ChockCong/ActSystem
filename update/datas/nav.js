d407ba30e0e9bfbca42312c9370d8c76db2441c6
Yelzi <Yelzi@YelOrange>
2017-02-21 19:06:47 +0800

    文件较多，重写路径不知道是哪个的话和我说

---
+++ update/datas/nav.js
@@ -0,0 +1,44 @@
+var navs = [{
+	"title": "活动管理",
+	"icon": "fa-cubes",
+	"spread": true,
+	"children": [{
+		"title": "活动发布",
+		"icon": "&#xe641;",
+		"href": "acissue.html"
+	}, {
+		"title": "通知发布",
+		"icon": "&#xe63c;",
+		"href": "notice.html"
+	}, {
+		"title": "活动总览",
+		"icon": "fa-tasks",
+		"href": "acall.html"
+	}, {
+//		"title": "活动评分",
+//		"icon": "fa-edit",
+//		"href": "score.html"
+//	}, {
+		"title": "活动审核",
+		"icon": "fa-check",
+		"href": "acpass.html"
+	}]
+}, {
+	"title": "学生管理",
+	"icon": "fa-cogs",
+	"spread": false,
+	"children": [{
+		"title": "学生查询",
+		"icon": "fa-navicon",
+		"href": "squery.html"
+	}, {
+		"title": "添加学生",
+		"icon": "fa-group",
+		"href": "addstu.html"
+	}]
+}, {
+	"title": "学院管理",
+	"icon": "fa-address-book",
+	"href": "admininfor.html",
+	"spread": false,
+}];
