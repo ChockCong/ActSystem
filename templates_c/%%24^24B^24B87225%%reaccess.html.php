b31423c5ed37d31e42d3f2e7f40faa2675824a5f
BC <Administrator@CAHL6RYYVEFUAJM>
2017-03-14 00:57:37 +0800

    改了自动判断活动完成情况，还有通知公告的后台

--- templates_c/%%24^24B^24B87225%%reaccess.html.php
+++ templates_c/%%24^24B^24B87225%%reaccess.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.30, created on 2017-03-12 14:56:39
+<?php /* Smarty version 2.6.30, created on 2017-03-13 02:58:51
          compiled from reaccess.html */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
 smarty_core_load_plugins(array('plugins' => array(array('function', 'Menu', 'reaccess.html', 18, false),)), $this); ?>
